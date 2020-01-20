c8fc4fea55d9c5ec2cd8106d941b32438f13df95
Etheram <francois681927@hotmail.fr>
2020-01-20 18:40:45 +0100

    Modify function write for understand

--- sources/ft_write.s
+++ sources/ft_write.s
@@ -6,7 +6,7 @@
 #    By: frfrey <frfrey@student.le-101.fr>          +:+   +:    +:    +:+      #
 #                                                  #+#   #+    #+    #+#       #
 #    Created: 2020/01/20 13:18:51 by frfrey       #+#   ##    ##    #+#        #
-#    Updated: 2020/01/20 16:52:34 by frfrey      ###    #+. /#+    ###.fr      #
+#    Updated: 2020/01/20 18:39:51 by frfrey      ###    #+. /#+    ###.fr      #
 #                                                          /                   #
 #                                                         /                    #
 # **************************************************************************** #
@@ -14,13 +14,15 @@
 section .text
 		global _ft_write
 
+_error:
+			mov rax, -1				; put -1 to rax
+			ret						; return rax (-1)
+
 _ft_write:							; fd = rdi, buffer = rsi, bytes = rdx
 			mov rax, 0x2000004		; write() syscall
 			syscall					; make the call
 			cmp rdx, 0				; cheak if bytes == 0
-			jne	end					; if bytes != 0 jump end
-			ret						; return Value bytes = rdx
+			jne	_error				; if bytes != 0 jump end
 
 end:
-		mov rax, -1					; put -1 to rax
-		ret							; return rax (-1)
+			ret						; return Value bytes = rdx
