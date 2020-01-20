78c071d3ab2b0b31169f7de2750066996d5a40ee
Etheram <francois681927@hotmail.fr>
2020-01-20 18:43:30 +0100

    modify archi of read

--- sources/ft_read.s
+++ sources/ft_read.s
@@ -6,7 +6,7 @@
 #    By: frfrey <frfrey@student.le-101.fr>          +:+   +:    +:    +:+      #
 #                                                  #+#   #+    #+    #+#       #
 #    Created: 2020/01/20 13:41:18 by frfrey       #+#   ##    ##    #+#        #
-#    Updated: 2020/01/20 16:52:45 by frfrey      ###    #+. /#+    ###.fr      #
+#    Updated: 2020/01/20 18:43:05 by frfrey      ###    #+. /#+    ###.fr      #
 #                                                          /                   #
 #                                                         /                    #
 # **************************************************************************** #
@@ -14,13 +14,16 @@
 section	.text
 		global _ft_read
 
+_error:
+		mov rax, -1				; put -1 on rax
+		ret						; return rax(-1)
+
 _ft_read:						; fd = rdi, buffer = rsi, bytes = rdx
 		mov rax, 0x2000003		; read() syscall
 		syscall					; make the call
 		cmp rdx, 0				; check if bytes == 0
-		jne end					; if byte != 0 jump _error
+		jne _error				; if byte != 0 jump _error
 		ret
 
 end:
-		mov rax, -1				; put -1 on rax
-		ret						; return rax(-1)
+		ret						; return rax
